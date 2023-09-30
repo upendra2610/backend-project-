4699a491daed341f6e339d46f65fbe9666241bcd
upendra2610 <upendra.singh2610@gmail.com>
2023-10-01 03:45:41 +0530

    @ResponseStatus for handlling exception and hide internal detail so added stacktrace=never in application.properties file

--- src/main/java/dev/upendra/productcatalog/exceptions/ControllerAdvices.java
+++ src/main/java/dev/upendra/productcatalog/exceptions/ControllerAdvices.java
@@ -11,24 +11,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerAdvices {
 
 
-    @ExceptionHandler(NotFoundExceptions.class)
-    private ResponseEntity<ExceptionDto> notFoundExceptionHandler(NotFoundExceptions notFoundExceptions){
-        return new ResponseEntity(
-                new ExceptionDto(HttpStatus.NOT_FOUND, notFoundExceptions.getMessage()),
-                HttpStatus.NOT_FOUND
-
-        );
-
-
-    }
-    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
-    private ResponseEntity<ExceptionDto> handleArrayIndexOutOfBound(
-            ArrayIndexOutOfBoundsException notFoundException
-    ) {
-
-        return new ResponseEntity(
-                new ExceptionDto(HttpStatus.NOT_FOUND, notFoundException.getMessage()),
-                HttpStatus.NOT_FOUND
-        );
-    }
+//    @ExceptionHandler(NotFoundExceptions.class)
+//    private ResponseEntity<ExceptionDto> notFoundExceptionHandler(NotFoundExceptions notFoundExceptions){
+//        return new ResponseEntity(
+//                new ExceptionDto(HttpStatus.NOT_FOUND, notFoundExceptions.getMessage()),
+//                HttpStatus.NOT_FOUND
+//
+//        );
+//
+//
+//    }
+//    @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+//    private ResponseEntity<ExceptionDto> handleArrayIndexOutOfBound(
+//            ArrayIndexOutOfBoundsException notFoundException
+//    ) {
+//
+//        return new ResponseEntity(
+//                new ExceptionDto(HttpStatus.NOT_FOUND, notFoundException.getMessage()),
+//                HttpStatus.NOT_FOUND
+//        );
+//    }
 }
--- src/main/java/dev/upendra/productcatalog/exceptions/NotFoundExceptions.java
+++ src/main/java/dev/upendra/productcatalog/exceptions/NotFoundExceptions.java
@@ -1,6 +1,12 @@
 package dev.upendra.productcatalog.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundExceptions extends Exception{
+
 
     public NotFoundExceptions(String message){
         super(message);
