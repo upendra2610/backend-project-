a23669f8d6585d0e2d77935a51c1c5a7ac09e55f
upendra2610 <upendra.singh2610@gmail.com>
2023-10-07 04:46:01 +0530

    creating UUID2, try to implement uuid4 with the help of google

--- src/main/java/dev/upendra/productcatalog/models/BaseModel.java
+++ src/main/java/dev/upendra/productcatalog/models/BaseModel.java
@@ -1,10 +1,19 @@
 package dev.upendra.productcatalog.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
+
     @Id
-    private long id;
+    @GeneratedValue(generator = "someuuid")
+    @GenericGenerator(name = "someuuid", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID uuid;
 }
